16b4f83bf568d754c1d2ddc922c2e64dd5c5c969
Jill Royer <hello@jillroyer.me>
2021-10-20 12:10:34 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,104 @@
+/*
+D'autres manières de représenter les différents coups possible.
+# propriétés d'un objet
+```
+let coupsPossibles = {
+    pierre: "pierre",
+    feuille: "feuille",
+    ciseau: "ciseau"
+}
+```
+Le coup pierre sera représenté par `coupsPossible.pierre`.
+# Tableau
+```
+listcoupsPossibles = ["pierre", "feuille", "ciseau"];
+```
+Le coup pierre sera représenté par listcoupsPossibles[0] : pas très lisible dans le code.
+*/
+
+function display(text) {
+    document.getElementById("textContent").innerHTML += text + "<br/>";
+}
+
+const PIERRE = "p";
+const FEUILLE = "f";
+const CISEAU = "c";
+const MANCHES_VICTORIEUSES = 3;
+
+
+function mancheGagnante(coup) {
+    let coupOrdinateur;
+    let hasard = Math.floor(Math.random() * 3);
+
+    switch (hasard) {
+        case 0:
+            coupOrdinateur = PIERRE;
+            break;
+        case 1:
+            coupOrdinateur = FEUILLE;
+            break;
+        case 2:
+            coupOrdinateur = CISEAU;
+            break;
+    }
+
+    if ((coup === FEUILLE && coupOrdinateur === PIERRE) ||
+        (coup === CISEAU && coupOrdinateur === FEUILLE) ||
+        (coup === PIERRE && coupOrdinateur === CISEAU)) {
+        /**
+         * coup - coupOrdinateur === 1 || coup - coupOrdinateur == -2
+         */
+        return true;
+    } else if (coup === coupOrdinateur) {
+        return null;
+    } else  {
+        return false;
+    }
+}
+
+
+let scoreJoueuse = 0;
+let scoreOrdi = 0
+
+
+// i = le compteur de manche
+// la boucle continue tant qu'aucun des joueurs n'a atteint 3
+function jouer(coup) {
+    let resultatManche = mancheGagnante(coup)
+    
+    if (resultatManche === true) {
+        // la joueuse a gagné
+        scoreJoueuse++;
+        display("Manche gagnée");
+    } else if (resultatManche === false) {
+        scoreOrdi++;
+        display("Manche perdue")
+    } else {
+        display("Manche nulle")
+    }
+
+    if (scoreJoueuse === MANCHES_VICTORIEUSES) {
+        display("La joueuse a gagné")
+    } else if (scoreOrdi === MANCHES_VICTORIEUSES) {
+        display("L'ordinateur a gagné")
+    }    
+}
+
+
+/*
+Les 3 possibilités pour passer une fonction en paramètre
+*/
+function jouerPierre() {
+    jouer(PIERRE)
+}
+
+document.getElementById("pierre")
+    .addEventListener("click", jouerPierre)
+
+document.getElementById("feuille")
+    .addEventListener("click", function() {
+        jouer(FEUILLE)
+    })
+
+document.getElementById("ciseau")
+    .addEventListener("click", () => jouer(CISEAU))
