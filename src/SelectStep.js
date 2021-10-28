64f86c87a122eb0c170a8acf85beafd431cf2290
Jill Royer <hello@jillroyer.me>
2021-10-28 11:32:30 +0200

    feat: center SelectStep title

--- src/SelectStep.js
+++ src/SelectStep.js
@@ -4,11 +4,27 @@ import { PIERRE, FEUILLE, CISEAU, } from "./index";
 const SelectStep = (props) => {
     const jouer = props.jouer;
 
-    return  <>
+    const containerStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%"
+    }
+
+    const titleStyle = {
+        fontSize: "86.2976px",
+        textTransform: "uppercase",
+        color: "#2818DF",
+        margin: 0,
+    }
+
+    return  <div style={containerStyle}>
+        <h1 style={titleStyle}>Choisis bien&nbsp;!</h1>
         <button onClick={() => jouer(PIERRE)}>Pierre</button>
         <button onClick={() => jouer(FEUILLE)}>Feuille</button>
         <button onClick={() => jouer(CISEAU)}>Ciseau</button>
-    </>
+    </div>
 }
 
 export default SelectStep;
